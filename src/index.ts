a1aeafa9390a4dff5d11cacbdbb44a37236aa3ad
Martijn van Arkel <cmvanarkel@hotmail.com>
2020-06-25 20:48:39 +0200

    small clean-up

--- src/index.ts
+++ src/index.ts
@@ -99,60 +99,7 @@ setInterval(() => {
         default:
             console.log(`Unknown game state '${gameStateManager.gameState}'!`)
     }
-}, 250);
-
-document.addEventListener('keydown', (event) => {
-    var keyArrowUp = 38;
-    var keyArrowDown = 40;
-    var keyArrowLeft = 37;
-    var keyArrowRight = 39;
-    var keyA = 65;
-    var keyD = 68;
-    var keyW = 87;
-    var keyS = 83;
-
-    var keysUsedInGame = [
-        keyArrowUp,
-        keyArrowDown,
-        keyArrowLeft,
-        keyArrowRight,
-        keyA,
-        keyD,
-        keyW,
-        keyS
-    ];
-
-    if(keysUsedInGame.indexOf(event.keyCode) > -1){
-        event.preventDefault();
-    }
-
-    /*switch(event.keyCode){
-        case keyArrowUp:
-        case keyW:
-            if(snake.velocity.y !== 1){
-                snake.velocity = { x: 0, y: -1 };
-            }
-            break;
-        case keyArrowDown:
-        case keyS:
-            if(snake.velocity.y !== -1){
-                snake.velocity = { x: 0, y: 1 };
-            }
-            break;
-        case keyArrowLeft:
-        case keyA:
-            if(snake.velocity.x !== 1){
-                snake.velocity = { x: -1, y: 0 };
-            }
-            break;
-        case keyArrowRight:
-        case keyD:
-            if(snake.velocity.x !== -1){
-                snake.velocity = { x: 1, y: 0 };
-            }
-            break;
-    }*/
-});
+}, 175);
 
 canvas.addEventListener('mousedown', (event) => {
     const canvasBoundingClientRect = canvas.getBoundingClientRect();
@@ -174,39 +121,3 @@ canvas.addEventListener('mousedown', (event) => {
             }
     }
 });
-/*
-var swipeStarted = true;
-var touchStartX: number, touchStartY: number;
-canvas.addEventListener('touchstart', (event) => {
-    if(gameStateManager.gameState === GameStates.PlayingGame){
-        event.preventDefault();
-        swipeStarted = true;
-        touchStartX = event.touches[0].clientX;
-        touchStartY = event.touches[0].clientY;
-    }
-
-});
-canvas.addEventListener('touchmove', (event) => {
-    if(gameStateManager.gameState === GameStates.PlayingGame && swipeStarted){
-        var diffX = touchStartX - event.touches[0].clientX;
-        var diffY = touchStartY - event.touches[0].clientY;
-        var swipteTreshold = 40;
-
-        if(Math.abs(diffX) > Math.abs(diffY)){
-            if(diffX > swipteTreshold){
-                gameWorld.snake.changeMovementDirection(SnakeDirectionChange.Left);
-            }else if(diffX < -swipteTreshold){
-                gameWorld.snake.changeMovementDirection(SnakeDirectionChange.Right);
-            }
-        }else{
-            if(diffY > swipteTreshold){
-                gameWorld.snake.changeMovementDirection(SnakeDirectionChange.Up);
-            }else if(diffY < -swipteTreshold){
-                gameWorld.snake.changeMovementDirection(SnakeDirectionChange.Down);
-            }
-        }
-    }
-});
-
-canvas.addEventListener("touchend", () => swipeStarted = false);
-canvas.addEventListener("touchcancel",() => swipeStarted = false);*/
